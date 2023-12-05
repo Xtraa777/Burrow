38d5ae45c2d9f55e0491fa3cbd8a864c35940cbd
Xtraa777 <mazino0588@gmail.com>
2023-12-05 16:08:49 +0900

    init commit

---
+++ src/main/java/com/burrow/burrow/BurrowApplication.java
@@ -0,0 +1,13 @@
+package com.burrow.burrow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BurrowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BurrowApplication.class, args);
+    }
+
+}
